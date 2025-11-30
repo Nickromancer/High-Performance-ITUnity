d0611744e92c2908829b6f10fc66515d64fa597b
Nicholas Hansen <nicholas.hansen101@gmail.com>
2025-11-30 17:31:13 +0100

    Fix collision

--- Assets/Scripts/Systems/ParticleSystem.cs
+++ Assets/Scripts/Systems/ParticleSystem.cs
@@ -12,17 +12,54 @@ partial struct ParticleSystem : ISystem
     public void OnCreate(ref SystemState state)
     {
         time = 0;
-        state.RequireForUpdate<ParticleSystemTag>();
+        state.RequireForUpdate<SpawnerComponent>();
     }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
+        var cam = SystemAPI.GetSingleton<CameraComp>();
         time += Time.deltaTime;
-        foreach (var velocity in SystemAPI.Query<RefRW<PhysicsVelocity>>().WithAll<ParticleTag>())
+        foreach (var (velocity, trans) in SystemAPI.Query<RefRW<PhysicsVelocity>, RefRW<LocalTransform>>().WithAll<ParticleTag>())
         {
             velocity.ValueRW.Linear = new float3(0, Physics.gravity.y, 0);
+
+            trans.ValueRW.Rotation = LookAt(trans.ValueRW.Position, cam.position);
         }
+    }
+
+    public static Quaternion LookAt(Vector3 sourcePoint, Vector3 destPoint)
+    {
+        Vector3 forwardVector = Vector3.Normalize(destPoint - sourcePoint);
+
+        float dot = Vector3.Dot(Vector3.forward, forwardVector);
+
+        if (math.abs(dot - (-1.0f)) < 0.000001f)
+        {
+            return new Quaternion(Vector3.up.x, Vector3.up.y, Vector3.up.z, 3.1415926535897932f);
+        }
+        if (math.abs(dot - (1.0f)) < 0.000001f)
+        {
+            return Quaternion.identity;
+        }
+
+        float rotAngle = (float)math.acos(dot);
+        Vector3 rotAxis = Vector3.Cross(Vector3.forward, forwardVector);
+        rotAxis = Vector3.Normalize(rotAxis);
+        return CreateFromAxisAngle(rotAxis, rotAngle);
+    }
+
+    // just in case you need that function also
+    public static Quaternion CreateFromAxisAngle(Vector3 axis, float angle)
+    {
+        float halfAngle = angle * .5f;
+        float s = (float)System.Math.Sin(halfAngle);
+        Quaternion q;
+        q.x = axis.x * s;
+        q.y = axis.y * s;
+        q.z = axis.z * s;
+        q.w = (float)System.Math.Cos(halfAngle);
+        return q;
     }
 
     [BurstCompile]
