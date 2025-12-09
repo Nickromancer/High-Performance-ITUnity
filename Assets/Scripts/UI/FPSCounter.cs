3d42a02d57d785205bb0d2a1f625156597d69c01
Weihao Nyholm-Andersen <weihaoandersen@gmail.com>
2025-12-09 14:08:30 +0100

    directly printing fps instead of caching the fps in dict

--- Assets/Scripts/UI/FPSCounter.cs
+++ Assets/Scripts/UI/FPSCounter.cs
@@ -49,15 +49,15 @@ public class FPSCounter : MonoBehaviour
 
         // Assign to UI
         {
-            string fpsString = _currentAveraged switch
-            {
-                var x when x >= 0 && x < _cacheNumbersAmount * 1000 => CachedNumberStrings[x],
-                var x when x >= _cacheNumbersAmount * 1000 => $"> {_cacheNumbersAmount * 1000}",
-                var x when x < 0 => "< 0",
-                _ => "?"
-            };
+            // string fpsString = _currentAveraged switch
+            // {
+            //     var x when x >= 0 && x < _cacheNumbersAmount * 1000 => CachedNumberStrings[x],
+            //     var x when x >= _cacheNumbersAmount * 1000 => $"> {_cacheNumbersAmount * 1000}",
+            //     var x when x < 0 => "< 0",
+            //     _ => "?"
+            // };
             float msPerFrame = _currentAveraged > 0 ? 1000f / _currentAveraged : 0f;
-            Text.text = $"FPS: {fpsString} | {msPerFrame:F2} ms";
+            Text.text = $"FPS: {_currentAveraged} | {msPerFrame:F2} ms";
             //Text.text = $"FPS: {currentFrame}";
         }
     }
